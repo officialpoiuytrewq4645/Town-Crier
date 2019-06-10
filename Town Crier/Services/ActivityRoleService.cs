517340356aefd89687c21b83d90f943bd9ff40b9
Joel <narmdo@gmail.com>
2019-06-10 13:15:53 +1000

    Trying to get roles to update properly on bot start

--- Town Crier/Services/ActivityRoleService.cs
+++ Town Crier/Services/ActivityRoleService.cs
@@ -20,110 +20,15 @@ namespace TownCrier
 		Listening = 1 << ActivityType.Listening,
 		Watching = 1 << ActivityType.Watching,
 	}
-
-	public class ActivityDefinition
-	{
-		readonly string nameRegex;
-		readonly string roleName;
-		readonly int activities;
-
-		Dictionary<IGuild, IRole> guildToRoleMap = new Dictionary<IGuild, IRole>();
-
-		public ActivityDefinition(string nameRegex, string roleName, ActivityFlag activities = (ActivityFlag)(-1))
-		{
-			this.nameRegex = nameRegex;
-
-			this.roleName = roleName;
-
-			this.activities = (int)activities;
-		}
-
-		public void InitializeForGuild(IGuild guild)
-		{
-			if (!guildToRoleMap.ContainsKey(guild))
-			{
-				IRole role = guild.Roles.FirstOrDefault(test => test.Name == roleName);
-
-				guildToRoleMap.Add(guild, role);
-			}
-		}
-
-		public async Task ApplyRole(IGuildUser user)
-		{
-			IRole role;
-
-			if (TryGetRole(user.Guild, out role))
-			{
-				await ApplyRole(role, user);
-			}
-		}
-
-		public async Task ApplyRole(SocketGuild guild)
-		{
-			IRole role;
-
-			if (TryGetRole(guild, out role))
-			{
-				foreach (IGuildUser user in guild.Users)
-				{
-					await ApplyRole(role, user);
-				}
-			}
-		}
-
-		public async Task RemoveRole(SocketGuild guild)
-		{
-			IRole role;
-
-			if (TryGetRole(guild, out role))
-			{
-				foreach (IGuildUser user in guild.Users)
-				{
-					await user.RemoveRoleAsync(role);
-				}
-			}
-		}
-
-		async Task ApplyRole(IRole role, IGuildUser user)
-		{
-			if (IsMatched(user.Activity))
-			{
-				if (!user.RoleIds.Contains(role.Id))
-				{
-					await user.AddRoleAsync(role);
-				}
-			}
-			else if (user.RoleIds.Contains(role.Id))
-			{
-				await user.RemoveRoleAsync(role);
-			}
-		}
-
-		bool TryGetRole(IGuild guild, out IRole role)
-		{
-			return guildToRoleMap.TryGetValue(guild, out role);
-		}
-
-		bool IsMatched(IActivity activity)
-		{
-			return
-				activity != null &&
-				(activities & (1 << (int)activity.Type)) != 0 &&
-				Regex.IsMatch(activity.Name, nameRegex, RegexOptions.IgnoreCase);
-		}
-	}
-
+	
 	public class ActivityRoleService
 	{
 		public bool IsEnabled { get;  set; }
 
-		 readonly DiscordSocketClient client;
-		 readonly IServiceProvider provider;
-		 readonly TownDatabase database;
-
-		List<ActivityDefinition> activities = new List<ActivityDefinition>();
-		List<SocketGuild> guilds = new List<SocketGuild>();
-
+		readonly DiscordSocketClient client;
+		readonly IServiceProvider provider;
+		readonly TownDatabase database;
+		
 		public ActivityRoleService(IServiceProvider provider, DiscordSocketClient client, TownDatabase database)
 		{
 			this.provider = provider;
@@ -132,10 +37,10 @@ namespace TownCrier
 
 			this.client.GuildMemberUpdated += UserUpdated;
 
-			this.client.Ready += Ready;
+			Ready();
 		}
 
-		async Task Ready()
+		async void Ready()
 		{
 			foreach (TownGuild guild in database.Guilds.FindAll())
 			{
@@ -147,7 +52,7 @@ namespace TownCrier
 				SocketGuild discordGuild = client.GetGuild(guild.GuildId);
 
 				await discordGuild.DownloadUsersAsync();
-
+				
 				foreach (SocketGuildUser user in discordGuild.Users)
 				{
 					await UpdateRoles(null, user, guild);
@@ -170,7 +75,9 @@ namespace TownCrier
 		{
 			if (oldUser == null || oldUser.Activity != null)
 			{
-				IEnumerable<ActivityRole> oldPotentialRoles = oldUser == null ? guild.ActivityRoles : guild.ActivityRoles.Where(x => x.ActivityType == oldUser.Activity.Type);
+				int activityFlag = oldUser == null ? 0 : 1 << (int)oldUser.Activity.Type;
+
+				IEnumerable<ActivityRole> oldPotentialRoles = oldUser == null ? guild.ActivityRoles : guild.ActivityRoles.Where(x => ((int)x.ActivityType & activityFlag) != 0);
 
 				// Removing old roles
 				foreach (var activity in oldPotentialRoles)
@@ -179,7 +86,7 @@ namespace TownCrier
 
 					if (role != null)
 					{
-						if (!Regex.IsMatch(newUser.Activity.Name, activity.ActivityName))
+						if (!Regex.IsMatch(newUser.Activity.Name, activity.ActivityName, RegexOptions.IgnoreCase))
 						{
 							await newUser.RemoveRoleAsync(role);
 						}
@@ -189,25 +96,22 @@ namespace TownCrier
 
 			if (newUser.Activity != null)
 			{
+				int activityFlag = 1 << (int)newUser.Activity.Type;
+
 				// Adding new roles
-				foreach (var activity in guild.ActivityRoles.Where(x => x.ActivityType == newUser.Activity.Type))
+				foreach (var activity in guild.ActivityRoles.Where(x => ((int)x.ActivityType & activityFlag) != 0))
 				{
 					IRole role = newUser.Guild.GetRole(activity.AssociatedRole);
 
 					if (!newUser.Roles.Contains(role))
 					{
-						if (Regex.IsMatch(newUser.Activity.Name, activity.ActivityName))
+						if (Regex.IsMatch(newUser.Activity.Name, activity.ActivityName, RegexOptions.IgnoreCase))
 						{
 							await newUser.AddRoleAsync(role);
 						}
 					}
 				}
 			}
-		}
-
-		bool RegexValidate(string activity, string regex)
-		{
-			return new Regex(regex).IsMatch(activity);
 		}
 	}
 }

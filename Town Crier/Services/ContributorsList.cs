8316a1205274c7db3f45eb7634d560af2d421363
officialpoiuytrewq4645 <poplive.com.au@gmail.com>
2020-07-26 23:38:48 +1000

    Updated ContributerList to use github api thus never requiring manual updating

--- Town Crier/Services/ContributorsList.cs
+++ Town Crier/Services/ContributorsList.cs
@@ -1,15 +1,20 @@
 ﻿using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Town_Crier.Services
 {
     class ContributorsList
     {
 		DiscordSocketClient discord;
+		HttpClient Client;
+		JsonSerializerSettings serializerSettings;
 
 		public ContributorsList(DiscordSocketClient discord)
 		{
@@ -17,26 +22,50 @@ namespace Town_Crier.Services
 			this.discord = discord;
 
 			discord.MessageReceived += Contributors;
+
+			Client = new HttpClient();
+			Client.DefaultRequestHeaders.Accept.Clear();
+			Client.DefaultRequestHeaders.Add("User-Agent", "Town Crier (Discord Bot)");
+
+			serializerSettings = new JsonSerializerSettings();
+			serializerSettings.MissingMemberHandling = MissingMemberHandling.Ignore;
 			
 		}
-
-		// Contributors, add your name to this list.
 
 		async Task Contributors(SocketMessage message)
 		{
 			if (message.Content.ToLower().Contains("!contributors"))
 			{
-				
+				//line break before user list
+				string contributerlist = "\n";
+				HttpResponseMessage ContributersResponseMessage = await Client.GetAsync("https://api.github.com/repos/alta-vr/Town-Crier/stats/contributors");
 
-				await message.Channel.SendMessageAsync("These are the people who helped make Town Crier! " +
-					"Narmdo (Joel_Alta), " +
-					"Timo_Alta, " +
-					"Mattssu, " +
-					"Klives, " +
-					"yoshisman8, " +
-					"Snoofo, " +
-					"officialpoiuytrewq4645. ");
+				
+				GithubResponseFormat[] Contributers = JsonConvert.DeserializeObject<GithubResponseFormat[]>(await ContributersResponseMessage.Content.ReadAsStringAsync(), serializerSettings);
+
+				foreach (GithubResponseFormat contributer in Contributers)
+				{
+					contributerlist += contributer.author.login;
+					contributerlist += "\n";
+				}
+
+
+				await message.Channel.SendMessageAsync("These are the people who helped make Town Crier! " + contributerlist);
 			}
 		}
+		// there is a lot more in this format but its unused currently so i cant be bothered implementing it
+		private struct GithubAuthorFormat
+		{
+			/// <summary>
+			/// practically just the username
+			/// </summary>
+			public string login;
+			public string type;
+		}
+		private struct GithubResponseFormat
+		{
+			public GithubAuthorFormat author;
+		}
 	}
+	
 }
